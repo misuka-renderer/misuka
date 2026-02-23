ddfc6cfd1fe16cba47f54093690b3ca7ed428613
Tobias Jüterbock <a.jueterbock@tu-berlin.de>
2026-02-23 16:21:05 +0100

    Use β_max > threshold instead of throughput β_max != 0

--- src/python/python/ad/integrators/acoustic_ad.py
+++ src/python/python/ad/integrators/acoustic_ad.py
@@ -480,7 +480,6 @@ class AcousticADIntegrator(RBIntegrator):
 
             # Don't run another iteration if the throughput has reached zero
             β_max = dr.max(β)
-            active_next &= (β_max != 0)
             active_next &= β_max >= self.energy_threshold
             active_next &= distance <= max_distance
 
--- src/python/python/ad/integrators/acoustic_prb.py
+++ src/python/python/ad/integrators/acoustic_prb.py
@@ -268,7 +268,6 @@ class AcousticPRBIntegrator(AcousticADIntegrator):
 
             # Don't run another iteration if the throughput has reached zero
             β_max = dr.max(β)
-            active_next &= (β_max != 0)
             active_next &= β_max >= self.energy_threshold
             active_next &= distance <= max_distance
 
