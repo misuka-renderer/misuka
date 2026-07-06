b06c3f406a684bf93508e4cb5a91d64aeb7a959a
Tobias Jüterbock <a.jueterbock@tu-berlin.de>
2026-07-06 18:33:12 +0200

    Fix zero-throughput path termination in Python acoustic integrators

--- src/python/python/ad/integrators/acoustic_ad.py
+++ src/python/python/ad/integrators/acoustic_ad.py
@@ -500,6 +500,7 @@ class AcousticADIntegrator(RBIntegrator):
 
             # Don't run another iteration if the throughput has reached zero
             β_max = dr.max(β)
+            active_next &= (β_max != 0)
             active_next &= β_max >= self.throughput_threshold
             active_next &= distance <= max_distance
 
